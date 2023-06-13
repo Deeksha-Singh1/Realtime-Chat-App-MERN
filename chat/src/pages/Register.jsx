80b89841d2447de0b39bc99f1e82f3fe10b4341f
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-06-13 17:03:46 +0530

    using react-toastify for validations

--- chat/src/pages/Register.jsx
+++ chat/src/pages/Register.jsx
@@ -1,17 +1,80 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import styled from 'styled-components'
-import {Link} from 'react-router-dom'
+import {Link,useNavigate} from 'react-router-dom'
 import Logo from '../assets/logo.svg'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
 
-const handleSubmit = (e)=>{
-  e.preventDefault();
-  alert("form");
-}
-const handleChange = (e)=>{
-  e.target.value();
-}
+  const navigate= useNavigate();
+
+  //properties for the error notification
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+
+  const[ values, setValues] = useState({
+    username:"",
+    email:"",
+    password:"",
+    confirmPassword:"", 
+  });
+
+  useEffect(()=>{
+    if(localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)){
+      navigate('/');
+    }
+  },[navigate]);
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if(handleValidation()){
+      const{email,username, password, confirmPassword}=values;
+     
+      alert(`hi`);
+    }
+  }
+
+
+  const handleChange = (e)=>{
+    setValues({...values,[e.target.name]:e.target.value});
+  }
+
+  const handleValidation = ()=>{
+    const{ password, confirmPassword, username,email }= values;
+
+    if(password !== confirmPassword){
+      toast.error(
+        "Password and Confirm Password should be same.",
+        toastOptions
+      );
+      return false;
+    }
+    else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (password.length < 5) {
+      toast.error(
+        "Password should be equal or greater than 5 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
+    }
+
+    return true;
+  };
 
   return (
     <>
@@ -27,12 +90,13 @@ const handleChange = (e)=>{
           <input type="email" placeholder='email' name='email' onChange={(e)=> handleChange(e)}/>
           <input type="password" placeholder='password' name='password' onChange={(e)=> handleChange(e)}/>
           
-          <input type="password" placeholder='password' name='password' onChange={(e)=> handleChange(e)}/>
+          <input type="password" placeholder='confirm password' name='confirmPassword' onChange={(e)=> handleChange(e)}/>
 
           <button type='submit'>Create User</button>
           <span>Already have an account ? <Link to='/login'>Login</Link></span>
         </form>
       </FormContainer>
+      <ToastContainer/>
     </>
   )
 }
