56a8ca57d23f6fd68dcc6a2e919efb3ce7b9b642
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-06-13 15:33:37 +0530

    Routes

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Chat from './pages/Chat';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={<Chat/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
