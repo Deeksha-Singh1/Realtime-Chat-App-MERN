7b4e56e3344726ec615beaaedbbc9d9b58b8eff9
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-06-13 15:08:49 +0530

    express connection

--- server/index.js
+++ server/index.js
@@ -6,3 +6,21 @@ const app = express();
 
 // used to load env variable from a '.env' file
 require("dotenv").config();
+
+//configure middleware
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=>{
+  console.log("db connected successfully");
+}).catch((err)=>{
+  console.log(err);
+});
+
+//starting server on port 5000
+const server = app.listen(process.env.PORT, ()=>{
+  console.log(`Server started on Port ${process.env.PORT}`);
+})
